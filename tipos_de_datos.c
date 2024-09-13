eb17b115a20314530f72d24f1cd249290230b784
edwin-ux-dev <edwin.ux.dev@gmail.com>
2024-09-13 15:59:58 +0000

    correciones para compilacion

--- tipos_de_datos.c
+++ tipos_de_datos.c
@@ -4,11 +4,11 @@ int main(){
     int num = 5;
     float dec = 1.25;
     double longNum = 15.123565478;
-    char letraA = a;
+    char letraA = 'a';
 
     printf("num con tipo de dato int tiene como valor : %d\n");
-    printf("dec con tipo de dato float tiene como valor : %d\n");
-    printf("longNum con tipo de dato double tiene como valor : %d\n");
-    printf("letraA con tipo de dato char tiene como valor : %d\n");
+    printf("dec con tipo de dato float tiene como valor : %.2f\n");
+    printf("longNum con tipo de dato double tiene como valor : %lf\n");
+    printf("letraA con tipo de dato char tiene como valor : %c\n");
     return 0;
 }
