f6713cb5180d22e3f67e814517a68114a5c06310
edwin-ux-dev <edwin.ux.dev@gmail.com>
2024-09-13 16:55:11 +0000

    experimento con sumas y bucle for

---
+++ experiment.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+int main(){
+
+int num = 0;
+printf("%d\n", num);
+
+
+num += 1;
+int num1;
+num1 = num;
+
+printf("%d\n", num);
+
+num += 2;
+int num2;
+num2 = num;
+
+printf("%d\n", num);
+
+
+
+num++;
+int num3;
+num3 = num;
+
+printf("num = %d\n", num);
+
+int suma = num1+num2+num3;  
+printf("suma : %d\n", suma);
+
+
+for (int i = 0; num; i++)
+{
+    //num -= 1;
+    printf("num = %d\n", num);
+}
+
+
+
+
+
+return (0);
+}
