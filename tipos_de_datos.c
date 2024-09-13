aa992f291f0688083c24224282449f738c29e0e6
edwin-ux-dev <edwin.ux.dev@gmail.com>
2024-09-13 09:35:26 -0500

    Declaracion de varibles de distintos tipos de datos e impresas por consola

---
+++ tipos_de_datos.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+    int num = 5;
+    float dec = 1.25;
+    double longNum = 15.123565478;
+    char letraA = a;
+
+    printf("num con tipo de dato int tiene como valor : %d\n");
+    printf("dec con tipo de dato float tiene como valor : %d\n");
+    printf("longNum con tipo de dato double tiene como valor : %d\n");
+    printf("letraA con tipo de dato char tiene como valor : %d\n");
+    return 0;
+}
